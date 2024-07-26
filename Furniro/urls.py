d881ad08aec0f802c394d37979a66a5068dd2da5
Saikal Ulukbekova <ukyzsaikal@gmail.com>
2024-07-26 12:11:01 +0200

    main app is ready-pages are ready

--- Furniro/urls.py
+++ Furniro/urls.py
@@ -20,5 +20,5 @@ from main import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls'), namespace="main")
+    path('', include('main.urls', namespace="main"))
 ]
