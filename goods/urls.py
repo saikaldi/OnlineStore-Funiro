53c7de13ac650849cffd367f8ce587ec459f42d0
Saikal Ulukbekova <ukyzsaikal@gmail.com>
2024-07-28 13:15:44 +0200

    Pagination done

--- goods/urls.py
+++ goods/urls.py
@@ -5,6 +5,5 @@ app_name = 'goods'
 
 urlpatterns = [
     path('<slug:category_slug>/', views.categories, name='index'),
-    path('<slug:category_slug>/<int:page>/', views.categories, name='index'),
     path('products/<slug:product_slug>/', views.products, name='products'),
 ]
