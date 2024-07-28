84f55fbe8b28b9c68b65d74164494e976b8b80e3
Saikal Ulukbekova <ukyzsaikal@gmail.com>
2024-07-28 15:12:22 +0200

    Search

--- goods/views.py
+++ goods/views.py
@@ -1,15 +1,21 @@
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, get_object_or_404, redirect, get_list_or_404
 from goods.models import Products, Categories
+from goods.utils import q_search
+
+
 # Create your views here.
 
 
-def categories(request, category_slug):
+def categories(request, category_slug=None):
 
     page = request.GET.get('page', 1)
+    query = request.GET.get('q', None)
 
     if category_slug == 'all':
         goods = Products.objects.all()
+    elif query:
+        goods = q_search(query)
     else:
         goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
 
