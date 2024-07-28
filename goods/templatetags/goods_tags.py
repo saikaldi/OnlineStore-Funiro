8e2ee2fb0e14e93ecac95d4cc88e88189b73ab2d
Saikal Ulukbekova <ukyzsaikal@gmail.com>
2024-07-28 13:31:40 +0200

    Added urlencode

--- goods/templatetags/goods_tags.py
+++ goods/templatetags/goods_tags.py
@@ -1,4 +1,6 @@
 from django import template
+from django.utils.http import urlencode
+
 from goods.models import Categories, Products
 
 register = template.Library()
@@ -16,3 +18,10 @@ def tag_products(context):
     request = context['request']
     products = Products.objects.all()
     return products
+
+
+@register.simple_tag(takes_context=True)
+def change_params(context, **kwargs):
+    query = context['request'].GET.dict()
+    query.update(kwargs)
+    return urlencode(query)
